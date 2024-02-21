e52b94b98e62843f4647aaaab612398ca14c1b8f
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-21 14:53:04 +0100

    add style

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
      <!--Bootstrap-->
      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-secondary">
     <header>
-        <section class="container pt-5 text-center">
+        <section class="container pt-5 text-center text-white">
            <h1>Strong Password Generator</h1>
            <h3>Genera una password sicura</h3>
         </section>
@@ -21,18 +21,15 @@
     </header>
     <main>
         <section class="container pt-3">
-            <form action="" method="GET" class="pt-3 px-5">
+            <form action="" method="GET" class="pt-3 px-5 bg-dark-subtle rounded">
                 <div class="pt-3 d-flex justify-content-between">
                    <label for="password">Lunghezza password:</label>
                    <input type="text" name="passord" id="password">
                 </div>
-                <div class="pt-3">
+                <div class="py-3">
                    <button type="button" class="btn btn-primary">Invia</button>
                    <button type="button" class="btn btn-secondary">Annulla</button>
                 </div>
-                
-
-                
             </form>
         </section>
     </main>
