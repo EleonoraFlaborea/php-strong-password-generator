7d00af40afa1e70b398ae549199c46c57720db7d
EleonoraFlaborea <flaboreaeleonora@gmail.com>
2024-02-21 14:41:30 +0100

    add structure form

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+     <!--Bootstrap-->
+     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+</head>
+<body>
+    <header>
+        <section class="container pt-5 text-center">
+           <h1>Strong Password Generator</h1>
+           <h3>Genera una password sicura</h3>
+        </section>
+       
+    </header>
+    <main>
+        <section class="container pt-3">
+            <form action="" method="GET" class="pt-3 d-flex justify-content-around">
+                <label for="password">Lunghezza password:</label>
+                <input type="text" name="passord" id="password">
+            </form>
+        </section>
+    </main>
+    
+</body>
+</html>
